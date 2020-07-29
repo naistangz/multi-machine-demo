f163b69a08ef370d4bfbf51b3efd58d8ce004c96
Anais <a6anaistang@hotmail.co.uk>
2020-07-29 12:06:22 +0200

    db/provision.sh

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -9,6 +9,7 @@ echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" 
 
 #Step 3: Refresh the local database with the packages
 sudo apt-get update -y
+sudo apt-get upgrade -y
 
 #Step 4: Install the last stable MongoDB version and all the necessary packages on our system
 sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
@@ -18,8 +19,8 @@ sudo rm /etc/mongod.conf
 sudo ln -s /home/vagrant/environment/db/mongod.conf /etc/mongod.conf
 
 #Step 5: Getting MongoDB set up
-sudo sed -i "s,\\(^[[:blank:]]*bindIp:\\) .*,\\1 0.0.0.0," /etc/mongod.conf
-sudo systemctl start mongod
+#sudo sed -i "s,\\(^[[:blank:]]*bindIp:\\) .*,\\1 0.0.0.0," /etc/mongod.conf
+sudo systemctl restart mongod
 sudo systemctl status mongod
 sudo systemctl enable mongod
 
